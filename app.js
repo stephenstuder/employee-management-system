44040fab17168d080eb458c4fa4fc7b8231f0dc6
Stephen Studer <stephenkstuder@gmail.com>
2020-05-20 13:59:41 -0700

    added inquirer initial prompt and banner

---
+++ app.js
@@ -0,0 +1,82 @@
+const mysql = require("mysql2/promise");
+const inquirer = require("inquirer");
+
+const main = async () => {
+    try {
+        const connection = await mysql.createConnection({
+            host: "localhost",
+            port: 3306,
+            user: "root",
+            password: "password",
+            database: "employee_managerDB",
+        });
+        console.log(`Connected to database with id ${connection.threadId}`);
+        //All the inquirier code goes here
+
+        //await readProducts(connection);
+        await runInquirer();
+        connection.end();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//add for each column
+
+//view for each column
+
+//update for employee role
+
+//update associated manager
+
+//delete departments, roles, and employees
+
+//view totalized budget of the department - ie combined salaries of all employees in the department
+
+const readProducts = async (connection) => {
+
+    //convert so that this joins all 3 tables
+
+    console.log("Selecting all rows ---------------")
+    
+    const [rows, fields] = await connection.query("SELECT * FROM department");
+    
+    console.table(rows);
+};
+
+//inquirer function that allows functionality to be visualized
+const runInquirer = async (connection) => {
+console.log(`
+.########.##.....##.########..##........#######..##....##.########.########....##.....##....###....##....##....###.....######...########.########.
+.##.......###...###.##.....##.##.......##.....##..##..##..##.......##..........###...###...##.##...###...##...##.##...##....##..##.......##.....##
+.##.......####.####.##.....##.##.......##.....##...####...##.......##..........####.####..##...##..####..##..##...##..##........##.......##.....##
+.######...##.###.##.########..##.......##.....##....##....######...######......##.###.##.##.....##.##.##.##.##.....##.##...####.######...########.
+.##.......##.....##.##........##.......##.....##....##....##.......##..........##.....##.#########.##..####.#########.##....##..##.......##...##..
+.##.......##.....##.##........##.......##.....##....##....##.......##..........##.....##.##.....##.##...###.##.....##.##....##..##.......##....##.
+.########.##.....##.##........########..#######.....##....########.########....##.....##.##.....##.##....##.##.....##..######...########.##.....##
+
+
+
+`)
+    let action = await inquirer.prompt(chooseAction);
+}
+
+const chooseAction = [{
+    type: "list",
+    name: "type",
+    message: "Welcome to employee manager, select an action you'd like to take",
+    choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "Exit Program"
+    ]
+}]
+
+
+
+main();
